e6ae37e850674efbdce4953553d50768938bb93b
Alex Guzey <alex.guzey@gmail.com>
2022-08-12 01:10:04 +0200

    deploy

---
+++ deploy/patch.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# final commit
+git add -A
+git commit -m 'deploy'
+
+# abort on errors
+set -e
+
+# update package version
+npm version patch
+
+# build
+npm run prepack
+
+# copy files to dist directory
+cp package.json dist
+cp LICENSE dist
+cp README.md dist
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# publish
+npm publish
+
+rm -rf .git
+
+cd -
